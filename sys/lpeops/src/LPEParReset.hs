9277ebd0fbd10aa34a4346cf3d2134b3d81d69a8
Djurre <djurrevanderwal@gmail.com>
2018-08-03 10:10:11 +0200

    Parameters that are temporarily not used can now be set to a default value

--- sys/lpeops/src/LPEParReset.hs
+++ sys/lpeops/src/LPEParReset.hs
@@ -27,10 +27,11 @@ import           LPEOps
 import           Satisfiability
 import           VarId
 import           ValExpr
+import           ConstDefs
 
 -- LPE rewrite method.
 -- Eliminates parameters that do not contribute to the behavior of a process from an LPE.
--- State spaces before and after are isomorph.
+-- State spaces before and after are strongly bisimilar.
 parReset :: LPEOperation
 parReset lpeInstance@((_channels, paramEqs, summands)) = do
     immediateSuccessors <- Monad.mapM (getImmediateSuccessors summands) summands
@@ -39,6 +40,7 @@ parReset lpeInstance@((_channels, paramEqs, summands)) = do
     return (Just newLPEInstance)
 -- parReset
 
+-- Selects all potential successors summands of a given summand from a list with all summands:
 getImmediateSuccessors :: [LPESummand] -> LPESummand -> IOC.IOC [LPESummand]
 getImmediateSuccessors _ LPEStopSummand = do return []
 getImmediateSuccessors allSummands (LPESummand _channelOffers _guard paramEqs) = do
@@ -55,29 +57,50 @@ getImmediateSuccessors allSummands (LPESummand _channelOffers _guard paramEqs) =
     addSummandIfImmediateSuccessor soFar _ = do return soFar
 -- getImmediateSuccessors 
 
+-- Updates the information collected about summands, in particular their lists of unused variables,
+-- until the information no longer changes.
+-- With the final information, assign ANY values to variables that are unused:
 parResetLoop :: LPEInstance -> [(LPESummand, [LPESummand], [VarId])] -> IOC.IOC LPEInstance
-parResetLoop lpeInstance successorsPerSummand = do
-    newSuccessorsPerSummand <- parResetUpdate successorsPerSummand
+parResetLoop lpeInstance@(channels, initParamEqs, summands) successorsPerSummand = do
     if newSuccessorsPerSummand == successorsPerSummand
-    then do return lpeInstance -- TODO do something with the instance!!
+    then do return (channels, initParamEqs, map updateSummand summands)
     else parResetLoop lpeInstance newSuccessorsPerSummand
+  where
+    newSuccessorsPerSummand = parResetUpdate successorsPerSummand
+    
+    updateSummand :: LPESummand -> LPESummand
+    updateSummand summand@(LPESummand channelOffers guard paramEqs) =
+        case [ uvars | (smd, _sucs, uvars) <- successorsPerSummand, smd == summand ] of
+          [uvars] -> LPESummand channelOffers guard (updateParamEqs paramEqs uvars)
+          _ -> LPESummand channelOffers guard paramEqs
+    updateSummand other = other
+    
+    updateParamEqs :: LPEParamEqs -> [VarId] -> LPEParamEqs
+    updateParamEqs paramEqs unusedVars = [ (p, if p `elem` unusedVars then cstrConst (Cany { sort = varsort p }) else v) | (p, v) <- paramEqs ]
 -- parResetLoop
 
-parResetUpdate :: [(LPESummand, [LPESummand], [VarId])] -> IOC.IOC [(LPESummand, [LPESummand], [VarId])]
-parResetUpdate successorsPerSummand = do return $ map checkSummand successorsPerSummand
+-- Updates the information collected about summands, in particular their lists of unused variables:
+parResetUpdate :: [(LPESummand, [LPESummand], [VarId])] -> [(LPESummand, [LPESummand], [VarId])]
+parResetUpdate successorsPerSummand = map updateSummand successorsPerSummand
   where
-    checkSummand :: (LPESummand, [LPESummand], [VarId]) -> (LPESummand, [LPESummand], [VarId])
-    checkSummand (summand@(LPESummand channelOffers guard _paramEqs), successors, unusedVars) =
+    -- Initially, all variables are added to the list of unused variables of a summand.
+    -- They are removed only if:
+    --  * They occur in the condition of the summand while not being defined as a communication variable, e.g. "CHANNEL ? x"; or
+    --  * They are used in the assignment to a variable that IS used by a potential successor summand.
+    updateSummand :: (LPESummand, [LPESummand], [VarId]) -> (LPESummand, [LPESummand], [VarId])
+    updateSummand (summand@(LPESummand channelOffers guard _paramEqs), successors, unusedVars) =
         let channelOfferVars = foldl (++) [] (map snd channelOffers) in
         let guardVars = FreeVar.freeVars guard in
         let locallyUnusedVars = unusedVars List.\\ (guardVars List.\\ channelOfferVars) in
-        let successorUnusedVars = [Set.fromList (uvars List.\\ (getSummandVarUsage smd uvars)) | (smd, _sucs, uvars) <- successorsPerSummand, suc <- successors, smd == suc ] in
+        let successorUnusedVars = [ Set.fromList (uvars List.\\ (getSummandVarUsage smd uvars)) | (smd, _sucs, uvars) <- successorsPerSummand, suc <- successors, smd == suc ] in
         let newUnusedVars = foldl Set.intersection (Set.fromList locallyUnusedVars) successorUnusedVars in
           (summand, successors, Set.toList newUnusedVars)
-    checkSummand other = other
+    updateSummand other = other
     
+    -- Of a given summand, determine which variables are used in its recursive call.
+    -- Assignments to variables that are considered unused (for now) are ignored!
     getSummandVarUsage (LPESummand _channelOffers _guard paramEqs) unusedVars =
-        foldl (++) [] [FreeVar.freeVars v | (p, v) <- paramEqs, not (p `elem` unusedVars)]
+        foldl (++) [] [ FreeVar.freeVars v | (p, v) <- paramEqs, not (p `elem` unusedVars) ]
     getSummandVarUsage _ _ = []
 -- parResetUpdate
 
