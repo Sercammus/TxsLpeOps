9b77b8dd9f4b616a46bf52a6331566863a5ac7da
Djurre <djurrevanderwal@gmail.com>
2018-10-19 17:02:39 +0200

    Modified satisfiability functionality to deal with undefined (work in progress)

--- sys/lpeops/src/utils/Satisfiability.hs
+++ sys/lpeops/src/utils/Satisfiability.hs
@@ -30,6 +30,7 @@ createVarSubst
 ) where
 
 import qualified Control.Monad as Monad
+import qualified Control.Monad.Identity as MonadId
 import qualified Control.Monad.State as MonadState
 import qualified EnvCore as IOC
 import qualified EnvData
@@ -50,7 +51,7 @@ import ValExpr
 import CstrId
 import LPEPrettyPrint
 import LPETypes
-import ValExprVisitor
+import ValExprVisitorM
 import VarFactory
 import ValFactory
 
@@ -75,9 +76,12 @@ restoreTdefs tdefs = do
 eliminateAny :: UndefVExpr -> IOC.IOC (TxsDefs.TxsDefs, UndefVExpr)
 eliminateAny (expr, undefs) = do
     tdefs <- MonadState.gets (IOC.tdefs . IOC.state)
-    newExpr <- visitValExprM anyElmVisitorM id Monad.Identity expr
+    newExpr <- visitValExprM anyElmVisitorM id buildVExpr expr
     return (tdefs, (newExpr, undefs))
   where
+    buildVExpr :: TxsDefs.VExpr -> IOC.IOC TxsDefs.VExpr
+    buildVExpr v = do return v
+    
     anyElmVisitorM :: [(TxsDefs.VExpr, Integer)] -> (TxsDefs.VExpr -> TxsDefs.VExpr) -> (TxsDefs.VExpr -> IOC.IOC TxsDefs.VExpr) -> TxsDefs.VExpr -> IOC.IOC TxsDefs.VExpr
     anyElmVisitorM subExps _ _ (view -> Vconst (Cany sort)) = do
         do varId <- createFreshVar sort
@@ -88,12 +92,12 @@ eliminateAny (expr, undefs) = do
 -- Applies a substitution to the given expression, introducing 'undefined variables' (as defined above) where necessary.
 -- Also returns the previous TorXakis definitions (so that they can be restored afterwards):
 doVarSubst :: [(VarId, UndefVExpr)] -> UndefVExpr -> IOC.IOC (TxsDefs.TxsDefs, UndefVExpr)
-doVarSubst substEqs expr = do
+doVarSubst substEqs (expr, undefs) = do
     tdefs <- MonadState.gets (IOC.tdefs . IOC.state)
-    (newExprs <- Monad.mapM eliminateAny (map snd substEqs)
+    newExprs <- Monad.mapM eliminateAny (map snd substEqs)
     let newSubstEqs = zip (map fst substEqs) (map snd newExprs)
-    newExpr <- visitValExprM substVisitor fst buildUndefVExpr expr
-    return (tdefs, newExpr)
+    (newExpr, newUndefs) <- visitValExprM substVisitor fst buildUndefVExpr expr
+    return (tdefs, (newExpr, undefs ++ newUndefs))
   where
     buildUndefVExpr :: TxsDefs.VExpr -> IOC.IOC UndefVExpr
     buildUndefVExpr v = do return (v, [])
@@ -120,124 +124,91 @@ doVarSubst substEqs expr = do
                 return (cstrVar varId, undefs2 ++ [varId])
     -- In other cases, the parent expression inherits undefined variables from its sub-expressions:
     substVisitor subExps g h (parentExpr, undefs) = do
-        parentExpr' <- defaultValExprVisitorM subExps g h (parentExpr, undefs)
-        return (parentExpr', undefs ++ (concat (map snd subExps)))
+        (parentExpr', undefs') <- defaultValExprVisitorM subExps g h (parentExpr, undefs)
+        return (parentExpr', undefs ++ undefs' ++ (concat (map (snd . fst) subExps)))
 -- doVarSubst
 
+extractVExprFromSolMap :: VarId -> Map.Map VarId Constant -> Constant
+extractVExprFromSolMap varId m = Map.findWithDefault (Cany (SortOf.sortOf varId)) varId m
+
+-- Frequently used method; code is modified code from TxsCore (with several safety checks removed!!).
+-- Attempts to find a solution for the given expression.
+-- If a solution is found, it consists of a map with one value for each of the specified variables.
+getSomeSolution :: UndefVExpr -> [VarId] -> IOC.IOC (SolveDefs.SolveProblem VarId)
+getSomeSolution (expr, undefs) variables =
+    if SortOf.sortOf expr /= SortId.sortIdBool
+    then do IOC.putMsgs [ EnvData.TXS_CORE_USER_ERROR ("Value expression must be of sort Bool (" ++ (show expr) ++ ")!") ]
+            return SolveDefs.UnableToSolve
+    else do smtEnv <- IOC.getSMT "current"
+            (tdefs1, (expr1, undefs1)) <- eliminateAny (expr, undefs)
+            let freeVars1 = Set.toList (Set.fromList ((FreeVar.freeVars expr1) ++ variables))
+            let assertions1 = Solve.add expr1 Solve.empty
+            (sol1, _) <- MonadState.lift $ MonadState.runStateT (Solve.solve freeVars1 assertions1) smtEnv
+            case sol1 of
+              SolveDefs.Solved solMap ->
+                do let freeVars2 = undefs
+                   let solvedVars = Set.toList ((Set.fromList freeVars1) Set.\\ (Set.fromList freeVars2))
+                   let solvedParamEqs = map (\v -> (v, (extractVExprFromMap v solMap, []))) solvedVars
+                   (_, solvedExpr) <- doVarSubst solvedParamEqs (expr1, undefs1)
+                   let assertions2 = Solve.add (cstrNot solvedExpr) Solve.empty
+                   (sol2, _) <- MonadState.lift $ MonadState.runStateT (Solve.solve freeVars2 assertions2) smtEnv
+                   case sol1 of
+                     SolveDefs.Unsolvable -> do restoreTdefs tdefs1
+                                                return (SolveDefs.Solved (Map.map (\v -> cstrConst (extractVExprFromSolMap v solMap)) variables))
+                     _ -> do restoreTdefs tdefs1
+                             return SolveDefs.UnableToSolve
+              otherResult -> do restoreTdefs tdefs1
+                                return otherResult
+-- getSomeSolution
+
 -- Checks if the specified expression cannot be false.
-isTautology :: TxsDefs.VExpr -> IOC.IOC Bool
-isTautology expression = isNotSatisfiable (cstrNot expression)
+isTautology :: UndefVExpr -> IOC.IOC Bool
+isTautology (expr, undefs) = isNotSatisfiable ((cstrNot expr), undefs)
 
 -- Checks if the specified expression can be true.
-isSatisfiable :: TxsDefs.VExpr -> IOC.IOC Bool
-isSatisfiable expression = do (tdefs, expr) <- eliminateAny expression
-                              sat <- getSat expr
-                              restoreTdefs tdefs
-                              case sat of
-                                SolveDefs.Sat -> return True
-                                _ -> return False
+isSatisfiable :: UndefVExpr -> IOC.IOC Bool
+isSatisfiable expr = do
+    sol <- getSomeSolution expr []
+    case sol of
+      SolveDefs.Solved _ -> do return True
+      _ -> do return False
 -- isSatisfiable
 
 -- Checks if the specified expression cannot be true.
-isNotSatisfiable :: TxsDefs.VExpr -> IOC.IOC Bool
-isNotSatisfiable expression = do (tdefs, expr) <- eliminateAny expression
-                                 sat <- getSat expr
-                                 restoreTdefs tdefs
-                                 case sat of
-                                   SolveDefs.Unsat -> return True
-                                   _ -> return False
+isNotSatisfiable :: UndefVExpr -> IOC.IOC Bool
+isNotSatisfiable expr = do
+    sol <- getSomeSolution expr []
+    return (sol == SolveDefs.UnableToSolve)
 -- isNotSatisfiable
 
 -- Checks if all specified expressions could be true.
 -- Note that each expression is considered in a vacuum, e.g. input [X == 0, X == 1] would yield true!
-areSatisfiable :: [TxsDefs.VExpr] -> IOC.IOC Bool
+areSatisfiable :: [UndefVExpr] -> IOC.IOC Bool
 areSatisfiable expressions = do sat <- Monad.mapM isSatisfiable expressions
                                 return (List.and sat)
 -- areSatisfiable
 
 -- Checks if none of the specified expressions not be true.
 -- Note that each expression is considered in a vacuum, e.g. input [X == 0, false] would yield false!
-areNotSatisfiable :: [TxsDefs.VExpr] -> IOC.IOC Bool
+areNotSatisfiable :: [UndefVExpr] -> IOC.IOC Bool
 areNotSatisfiable expressions = do sat <- Monad.mapM isNotSatisfiable expressions
                                    return (List.and sat)
 -- areNotSatisfiable
 
--- Frequently used method; code is modified code from TxsCore.
--- Checks whether the given expression is satisfiable.
-getSat :: TxsDefs.VExpr -> IOC.IOC SolveDefs.SolvableProblem
-getSat expression = do
-    envc <- MonadState.get
-    case IOC.state envc of
-      IOC.Noning -> do IOC.putMsgs [ EnvData.TXS_CORE_USER_ERROR "No 'satsolve' without model" ]
-                       return SolveDefs.Unknown
-      _ -> if SortOf.sortOf expression /= SortId.sortIdBool
-           then do IOC.putMsgs [ EnvData.TXS_CORE_USER_ERROR "Value expression shall be Bool" ]
-                   return SolveDefs.Unknown
-           else do let frees = FreeVar.freeVars expression
-                   let assertions = Solve.add expression Solve.empty
-                   smtEnv <- IOC.getSMT "current"
-                   case smtEnv of
-                     SMTData.SmtEnvError -> do IOC.putMsgs [ EnvData.TXS_CORE_ANY "Could not locate SMT solver" ]
-                                               return SolveDefs.Unknown
-                     _ -> if Solve.isEasySolve frees assertions
-                          then do (sat, smtEnv') <- MonadState.lift $ MonadState.runStateT (Solve.satSolve frees assertions) smtEnv
-                                  IOC.putSMT "current" smtEnv'
-                                  return sat
-                          else do IOC.putMsgs [ EnvData.TXS_CORE_ANY ("SMT log: " ++ (showValExpr expression) ++ " ==> ?") ]
-                                  (sat, smtEnv') <- MonadState.lift $ MonadState.runStateT (Solve.satSolve frees assertions) smtEnv
-                                  IOC.putMsgs [ EnvData.TXS_CORE_ANY ("SMT log: " ++ (showValExpr expression) ++ " ==> " ++ (show sat)) ]
-                                  IOC.putSMT "current" smtEnv'
-                                  return sat
--- getSat
-
--- Frequently used method; code is modified code from TxsCore.
--- Attempts to find a solution for the given expression.
-getSomeSolution :: TxsDefs.VExpr -> [VarId] -> IOC.IOC (Maybe (TxsDefs.TxsDefs, Map.Map VarId TxsDefs.VExpr))
-getSomeSolution expression variables = do
-    envc <- MonadState.get
-    case IOC.state envc of
-      IOC.Noning -> do IOC.putMsgs [ EnvData.TXS_CORE_USER_ERROR "No 'solve' without model" ]
-                       return Nothing
-      _ -> if SortOf.sortOf expression /= SortId.sortIdBool
-           then do IOC.putMsgs [ EnvData.TXS_CORE_USER_ERROR "Value expression shall be Bool" ]
-                   return Nothing
-           else do (tdefs, expr) <- eliminateAny expression
-                   let frees = Set.toList (Set.fromList ((FreeVar.freeVars expr) ++ variables))
-                   let assertions = Solve.add expr Solve.empty
-                   smtEnv <- IOC.getSMT "current"
-                   case smtEnv of
-                     SMTData.SmtEnvError -> do IOC.putMsgs [ EnvData.TXS_CORE_ANY "Could not locate SMT solver" ]
-                                               return Nothing
-                     _ -> if Solve.isEasySolve frees assertions
-                          then do (sol, smtEnv') <- MonadState.lift $ MonadState.runStateT (Solve.solve frees assertions) smtEnv
-                                  IOC.putSMT "current" smtEnv'
-                                  case sol of
-                                    SolveDefs.Solved solMap -> return (Just (tdefs, Map.map cstrConst solMap))
-                                    _ -> return Nothing
-                          else do IOC.putMsgs [ EnvData.TXS_CORE_ANY ("SMT log: " ++ (showValExpr expr) ++ " ==> ?") ]
-                                  (sol, smtEnv') <- MonadState.lift $ MonadState.runStateT (Solve.solve frees assertions) smtEnv
-                                  IOC.putMsgs [ EnvData.TXS_CORE_ANY ("SMT log: " ++ (showValExpr expr) ++ " ==> " ++ (showSolution sol)) ]
-                                  IOC.putSMT "current" smtEnv'
-                                  case sol of
-                                    SolveDefs.Solved solMap -> return (Just (tdefs, Map.map cstrConst solMap))
-                                    _ -> return Nothing
--- getSomeSolution
-
 -- Attempts to find a unique solution for the given expression.
 -- The solution only has to be unique with regard to the variables listed by the third parameter:
-getUniqueSolution :: TxsDefs.VExpr -> [VarId] -> [VarId] -> IOC.IOC (Maybe (TxsDefs.TxsDefs, Map.Map VarId TxsDefs.VExpr))
-getUniqueSolution expression variables uniquelySolvedVars = do
-    -- Start by finding some arbitrary solution:
-    (tdefs, expressionWithoutAny) <- eliminateAny expression
-    someSolution <- getSomeSolution expressionWithoutAny variables
-    case someSolution of
-      Just (_, solMap) -> do -- Then check if there is NO solution where (one of) the specified variables have different values:
-                             let extraConditions = map (\v -> cstrEqual (cstrVar v) (extractVExprFromMap v solMap)) uniquelySolvedVars
-                             let restrictedExpression = cstrAnd (Set.fromList [expressionWithoutAny, cstrNot (cstrAnd (Set.fromList extraConditions))])
-                             unsat <- isNotSatisfiable restrictedExpression
-                             -- If so, the solution is unique (w.r.t. the specified variables):
-                             return (if unsat then Just (tdefs, solMap) else Nothing)
-      _ -> return Nothing
+getUniqueSolution :: UndefVExpr -> [VarId] -> [VarId] -> IOC.IOC (SolveDefs.SolveProblem VarId)
+getUniqueSolution (expr, undefs) variables uniqueSolVars = do
+    sol <- getSomeSolution (expr, undefs) variables
+    case sol of
+      SolveDefs.Solved solMap ->
+        do -- Then check if there is NO solution where (one of) the specified variables have different values:
+           let extraConditions = map (\v -> cstrEqual (cstrVar v) (extractVExprFromSolMap v solMap)) uniqueSolVars
+           let restrictedExpression = (cstrAnd (Set.fromList [expr, cstrNot (cstrAnd (Set.fromList extraConditions))]), undefs)
+           unsat <- isNotSatisfiable restrictedExpression
+           -- If so, the solution is unique (w.r.t. the specified variables):
+           return (if unsat then sol else SolveDefs.UnableToSolve)
+      _ -> return sol
 -- getUniqueSolution
 
 showSolution :: SolveDefs.SolveProblem VarId -> String
