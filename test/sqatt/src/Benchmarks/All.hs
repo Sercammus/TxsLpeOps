d9b0c3aaa75ac2340ec3f6c2a53f290ca389141b
Djurre <djurrevanderwal@gmail.com>
2018-12-14 09:40:42 +0100

    Added more test cases to benchmark

--- test/sqatt/src/Benchmarks/All.hs
+++ test/sqatt/src/Benchmarks/All.hs
@@ -14,7 +14,7 @@ module Benchmarks.All (allExamples, allBenchmarks) where
 --import qualified Benchmarks.LPEAdder3         as LPEAdder3
 --import qualified Benchmarks.LPEBakery         as LPEBakery
 --import qualified Benchmarks.LPEMAdder         as LPEMAdder
-import qualified Benchmarks.LPEReadWrite      as LPEReadWrite
+import qualified Benchmarks.LPEBenchmarkSets      as LPEBenchmarkSets
 --import qualified Benchmarks.LPELuckyPeople    as LPELuckyPeople
 --import qualified Benchmarks.Queue           as Queue
 --import qualified Benchmarks.RealWorld       as RealWorld
@@ -28,10 +28,19 @@ allExamples = [ --Choice.benchmarksSet
               --, Dynamic.benchmarksSet
               --, Enable.benchmarksSet
               --, Hiding.benchmarksSet
-              --  LPEBakery.benchmarksSet
               --, LPEMAdder.benchmarksSet
-              LPEReadWrite.benchmarksSet
-              --, LPELuckyPeople.benchmarksSet
+              LPEBenchmarkSets.lpeBenchmarkSet "AdderModel",
+              LPEBenchmarkSets.lpeBenchmarkSet "Adder3Model",
+              LPEBenchmarkSets.lpeBenchmarkSet "MAdderModel",
+              LPEBenchmarkSets.lpeBenchmarkSet "BakeryModel",
+              --LPEBenchmarkSets.lpeBenchmarkSet "CustomersOrdersModel", -- Does not terminate!
+              LPEBenchmarkSets.lpeBenchmarkSet "EchoModel",
+              LPEBenchmarkSets.lpeBenchmarkSet "EchoDelayModel",
+              LPEBenchmarkSets.lpeBenchmarkSet "EchoLPEModel",
+              LPEBenchmarkSets.lpeBenchmarkSet "PointModel",
+              LPEBenchmarkSets.lpeBenchmarkSet "QueueModel",
+              LPEBenchmarkSets.lpeBenchmarkSet "LossyQueueModel",
+              LPEBenchmarkSets.lpeBenchmarkSet "ReadWriteModel"
               --, RealWorld.benchmarksSet
               --, Sequence.benchmarksSet
               --, Synchronization.benchmarksSet
---
+++ test/sqatt/src/Benchmarks/LPEBenchmarkSets.hs
@@ -0,0 +1,46 @@
+{-
+TorXakis - Model Based Testing
+Copyright (c) 2015-2017 TNO and Radboud University
+See LICENSE at root directory of this repository.
+-}
+{-# LANGUAGE OverloadedStrings #-}
+module Benchmarks.LPEBenchmarkSets (lpeBenchmarkSet) where
+
+import           Benchmarks.Common
+import           Paths
+import           Prelude           hiding (FilePath)
+import           Sqatt
+import qualified Data.Text as Text
+
+benchDir :: FilePath
+benchDir = "LPE"
+
+lpeBenchmarkSet :: String -> TxsExampleSet
+lpeBenchmarkSet coreName =
+    TxsExampleSet (fromString ("LPE" ++ coreName)) [ example1, example2, example3 ]
+  where
+    example1 :: TxsExample
+    example1 = emptyExample
+        { exampleName = "Original" ++ coreName
+        , txsModelFiles = [ txsFilePath BenchTest benchDir (Text.pack ("Original" ++ coreName)) ]
+        , txsCmdsFiles = [ seedSetupCmdFile, txsCmdPath BenchTest benchDir "Test" ]
+        , expectedResult = Pass
+        }
+
+    example2 :: TxsExample
+    example2 = emptyExample
+        { exampleName = "Unchanged" ++ coreName
+        , txsModelFiles = [ txsFilePath BenchTest benchDir (Text.pack ("Unchanged" ++ coreName)) ]
+        , txsCmdsFiles = [ seedSetupCmdFile, txsCmdPath BenchTest benchDir "Test" ]
+        , expectedResult = Pass
+        }
+
+    example3 :: TxsExample
+    example3 = emptyExample
+        { exampleName = "Reduced" ++ coreName
+        , txsModelFiles = [ txsFilePath BenchTest benchDir (Text.pack ("Reduced" ++ coreName)) ]
+        , txsCmdsFiles = [ seedSetupCmdFile, txsCmdPath BenchTest benchDir "Test" ]
+        , expectedResult = Pass
+        }
+-- lpeBenchmarkSet
+
